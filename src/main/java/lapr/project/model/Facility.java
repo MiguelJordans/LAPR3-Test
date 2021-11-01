4e6f34017a297fcd12ad0f1da2c2ee196daa9129
edumi8 <eduardoapsoua@gmail.com>
2021-11-01 12:34:15 +0000

    Revert "src/main/java/lapr/project/model/Facility.java edited online with Bitbucket"
    This reverts commit 8bfebdb427e47d3c98062a7742daf183b494c065.

---
+++ src/main/java/lapr/project/model/Facility.java
@@ -0,0 +1,76 @@
+package lapr.project.model;
+
+public class Facility {
+    private String identification;
+    private String name;
+    private String continent;
+    private String country;
+    private FacilityLocation location;
+
+    public Facility(String identification, String name, String continent, String country, FacilityLocation location) {
+        this.identification = identification;
+        this.name = name;
+        this.continent = continent;
+        this.country = country;
+        this.location = location;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Facility)) return false;
+        Facility facility = (Facility) o;
+        return getIdentification().equals(facility.getIdentification()) && getName().equals(facility.getName()) && getContinent().equals(facility.getContinent()) && getCountry().equals(facility.getCountry()) && getLocation().equals(facility.getLocation());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getIdentification() != null ? getIdentification().hashCode() : 0;
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
+        result = 31 * result + (getContinent() != null ? getContinent().hashCode() : 0);
+        result = 31 * result + (getCountry() != null ? getCountry().hashCode() : 0);
+        result = 31 * result + (getLocation() != null ? getLocation().hashCode() : 0);
+        return result;
+    }
+
+    public String getIdentification() {
+        return identification;
+    }
+
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContinent() {
+        return continent;
+    }
+
+    public void setContinent(String continent) {
+        this.continent = continent;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public FacilityLocation getLocation() {
+        return location;
+    }
+
+    public void setLocation(FacilityLocation location) {
+        this.location = location;
+    }
+}
