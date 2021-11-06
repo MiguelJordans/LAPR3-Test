8a2e3cb281bed440a0587335ddc7d8bca22eb0e1
edumi8 <eduardoapsoua@gmail.com>
2021-11-06 00:25:50 +0000

    Commit massivo com a minha us basicamente e com as mudanças que falei ao tiago e ao jordão, ele nao compila por causa de eu ter posto as duas stores juntas é preciso implementar os outros metodos a partir da bst

--- src/main/java/lapr/project/controller/App.java
+++ src/main/java/lapr/project/controller/App.java
@@ -2,20 +2,22 @@ package lapr.project.controller;
 
 
 import lapr.project.model.*;
-import lapr.project.utils.auth.AuthFacade;
 import lapr.project.shared.Constants;
+import lapr.project.utils.auth.AuthFacade;
 import lapr.project.utils.auth.UserSession;
 import lapr.project.utils.auth.domain.OrgRole;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Properties;
 
 public class App {
 
 
+    private static App singleton = null;
     private final Company company;
     private final AuthFacade authFacade;
 
@@ -29,8 +31,16 @@ public class App {
 
     }
 
-    public Company getCompany()
-    {
+    public static App getInstance() {
+        if (singleton == null) {
+            synchronized (App.class) {
+                singleton = new App();
+            }
+        }
+        return singleton;
+    }
+
+    public Company getCompany() {
         return this.company;
     }
 
@@ -65,7 +75,6 @@ public class App {
         this.authFacade.doLogout();
     }
 
-
     private void bootstrap() {
 
         this.authFacade.addUserRole(Constants.ROLE_CLIENT, Constants.ROLE_CLIENT);
@@ -83,31 +92,28 @@ public class App {
         this.authFacade.addUserWithRole(tm1.getName(), tm1.getEmail(), "495", Constants.ROLE_TRAFFIC_MANAGER);
 
         //Position
-        Position posgeral = new Position(0, 0, 0, 1, 0,null);
-        Position posgeral2 = new Position(20, 30, 20, 10, 10,null);
+        String sdate = "31/12/2020 23:16";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        LocalDateTime date = (LocalDateTime) formatter.parse(sdate);
+
+        String sdate2 = "31/12/2020 23:50";
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        LocalDateTime date2 = (LocalDateTime) formatter.parse(sdate);
+
+        Position posgeral = new Position(date, 0, 0, 0, 1, 0);
+        Position posgeral2 = new Position(date2, 20, 30, 20, 10, 10);
 
         //Ships
 
         //mmsi: 111111111
-        Ship shipTest1 = new Ship(111111111, "name", 1111111, 1, 1, "A", "A", 1, 1, 1, 1);
-        shipTest1.addNewPosMessage(new Date("Dec 31 1998 23:37:50"),posgeral);
-        shipTest1.addNewPosMessage(new Date("Dec 31 1997 23:37:50"),posgeral2);
+        Ship shipTest1 = new Ship(111111111, "name", "IMO1111111", 1, 1, "A", "A", 1, 1, 1, 1);
+/*        shipTest1.addNewPosMessage(new Date("Dec 31 1998 23:37:50"), posgeral);
+        shipTest1.addNewPosMessage(new Date("Dec 31 1997 23:37:50"), posgeral2);*/
         company.getShipStore().getlShip().add(shipTest1);
 
         //mmsi: 222222222
-        Ship shipTest2 = new Ship(222222222, "barquito", 1111111, 1, 1, "A", "A", 1, 1, 1, 1);
+        Ship shipTest2 = new Ship(222222222, "barquito", "IMO1111111", 1, 1, "A", "A", 1, 1, 1, 1);
         company.getShipStore().getlShip().add(shipTest2);
-    }
-
-    private static App singleton = null;
-
-    public static App getInstance() {
-        if (singleton == null) {
-            synchronized (App.class) {
-                singleton = new App();
-            }
-        }
-        return singleton;
     }
 
     public AuthFacade getAuthFacade() {
