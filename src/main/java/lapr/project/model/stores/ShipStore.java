f84c4e9bd37e5ce82715daa54c270362f6c49f35
DESKTOP-77R6829\Tiago Ferreira <1200601@isep.ipp.pt>
2021-11-06 12:25:09 +0000

    Hehehe not hehehehe

--- src/main/java/lapr/project/model/stores/ShipStore.java
+++ src/main/java/lapr/project/model/stores/ShipStore.java
@@ -3,6 +3,10 @@ package lapr.project.model.stores;
 import lapr.project.model.Ship;
 import lapr.project.shared.BinarySearchTree;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class ShipStore {
     BinarySearchTree<Ship> shipBinarySearchTree;
 
@@ -13,6 +17,29 @@ public class ShipStore {
     public ShipStore(BinarySearchTree<Ship> shipBinarySearchTree) {
         this.shipBinarySearchTree = shipBinarySearchTree;
     }
+
+
+    public void addShip(Ship ship){
+        this.shipBinarySearchTree.insert(ship);
+    }
+
+    public Ship getShipByMMSI(int mmsi){
+
+
+        Iterable<Ship> ls = shipBinarySearchTree.inOrder();
+
+        List<Ship> lista = new ArrayList<>();
+        ls.iterator().forEachRemaining(lista::add);
+
+        for(Ship s : lista){
+            if(s.getMmsi() == mmsi)
+                return s;
+        }
+
+
+        return null;
+    }
+
 
 
 }
